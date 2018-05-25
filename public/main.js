ec26d521a00bb37b8c12576eeb19cc1a7ad8598d
David Fisher <fisherds@rose-hulman.edu>
2018-05-25 07:45:26 -0400

    last minute departure cleaning

--- public/main.js
+++ public/main.js
@@ -2,9 +2,16 @@ var buttonClicks = 0;
 var isShowingNotes = false;
 
 $(document).ready(() => {
+
+    // TODO: Remove these lines!
+    $(".hidden-notes").show();
+    isShowingNotes = true;
+
+
     console.log("Click the magic button to show the notes.");
     $("#secret-button").click( ()=> {
         buttonClicks += 1;
+        // CONSIDER: This could be a lot shorter with a toggle.
         console.log("button clicks = " + buttonClicks);
         if (buttonClicks % 3 == 0) {
             isShowingNotes = !isShowingNotes;
